b78acc095eced379aa8c2f272df641d7b9da8c6b
Valerio Proietti <kamicane@gmail.com>
2012-02-29 22:47:22 +0100

    - modules are now called with exports as context, like in node (ty `sebmarkbage`).

--- includes/require.js
+++ includes/require.js
@@ -2,6 +2,7 @@ var cache = {}, require = function(id){
 	var module;
 	if (module = cache[id]) return module.exports;
 	module = cache[id] = {exports: {}};
-	modules[id](module, require, window, module.exports);
+	var exports = module.exports;
+	modules[id].call(exports, require, module, exports, window);
 	return module.exports;
 };
