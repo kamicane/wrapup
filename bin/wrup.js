160d6c96d6e8b637a11b1e33a2373fdcd9ab0272
Valerio Proietti <kamicane@gmail.com>
2012-03-06 03:38:32 +0100

    - .module is now (path, namespace) instead of (namespace, path), namespace is optional, defaulting to file name.  - using emi for events  - updated to clint 0.0.4 / emi 0.0.3  - removed submodules, use npm.

--- bin/wrup.js
+++ bin/wrup.js
@@ -5,6 +5,43 @@ var wrup = require("../lib/main")(),
 	fs = require("fs"),
 	json = require("../package")
 
+
+// consolize wrup errors
+
+wrup.on("error:js", function(module, from, err){
+	console.error("  ERROR: the module %s required by %s had a javascript error: %j", module, from || 'you', err)
+})
+
+wrup.on("error:resolve", function(module, from){
+	console.error("  ERROR: the module %s required by %s could not be resolved", module, from || 'you')
+})
+
+wrup.on("error:native", function(module, from){
+	console.error("  ERROR: the module %s required by %s is a native require", module, from || 'you')
+})
+
+wrup.on("error:package", function(pkg){
+	console.error("  ERROR: the package %s could not be resolved", pkg)
+})
+
+wrup.on("error:namespace", function(namespace){
+	console.error("  ERROR: the namespace %s was already in use", namespace)
+})
+
+wrup.on("warning:access", function(){
+	console.error("  WARNING: both --globalize and --wrup are turned off, you might not be able to access the required modules")
+})
+
+wrup.on("error:empty", function(){
+	console.error("  ERROR: no modules required")
+})
+
+wrup.on("error:internal", function(err){
+	console.error("  ERROR: kami, fix WrapUp because it doesnt work: %j", err)
+})
+
+// header
+
 console.warn("   , , , __  __.  _   . . _  ")
 console.warn("  (_(_/_/ (_(_/|_/_)_(_/_/_)_")
 console.warn("                /       /  " + json.version + "\n")
@@ -14,29 +51,30 @@ var specify = function(value){
 	return true
 }
 
-clint.option('--help', '-h',
+clint.command('--help', '-h',
              'general usage information.')
 
-clint.option('--version',
+clint.command('--version',
              '-v', 'prints the version number.')
 
-clint.option('--module', '-m',
-             'a manually named module to require <name /path/to/module> or <name package>')
+clint.command('--module', '-m',
+             'a manually named module to require, accepts a single path or path and namespace. `-m path/to/moofx3 fx` and `-m path/to/othermodule`')
 
-clint.option('--package', '-p',
-             'a valid package to require </path/to> or </path/to/package.json> or <package>')
+clint.command('--package', '-p',
+             'valid package / packages to require, accepts multiple paths.' +
+             ' `-p` for cwd(), or `-p path/to/package` or `-p path/to/package1 path/to/package2 path/to/package3 ...`')
 
-clint.option('--compress', '-c',
-             'compresses output using uglify-js mangle and squeeze. defaults to no|false.', specify)
+clint.command('--compress', '-c',
+             'compresses output using uglify-js mangle and squeeze. defaults to no|false, `-c` or `-c yes` to enable.', specify)
 
-clint.option('--wrup', '-w',
-             'includes the wrup client, to retrieve named modules as wrup(name).', specify)
+clint.command('--wrup', '-w',
+             'includes the wrup client, to retrieve required namespaces with `wrup(namespace)`. defaults to no|false, `-w yes` to enable.', specify)
 
-clint.option('--globalize', '-g',
-             'sets the mains of the modules you specifically required to the global scope. defaults to no|false.', specify)
+clint.command('--globalize', '-g',
+             'defined namespaces go to global scope. defaults to yes|true, `-g no` to disable.', specify)
 
-clint.option('--output', '-o',
-             'wraps up the contents of your modules to the specified file, instead of stdout.')
+clint.command('--output', '-o',
+             'wraps up the contents of your required modules to the specified filename, instead of stdout. `-o path/to/file`')
 
 var help = function(err){
 	console.log(clint.help())
@@ -47,49 +85,44 @@ var args = process.argv.slice(2)
 
 if (!args.length) help(1)
 
-clint.on('--help', function(){
+clint.on('command(--help)', function(){
 	help(0)
 })
 
-clint.on('--version', function(){
+clint.on('command(--version)', function(){
 	console.log("\n  " + json.version + "\n")
 	process.exit(0)
 })
 
-var modname, pass = false
+var pass = false
 
-clint.on('--module', function(required){
-	if (!modname){
-		modname = required
-	} else {
-		pass = true
-		wrup.module(modname, required)
-		modname = null
-	}
+clint.on('chunk(--module)', function(module, namespace){
+	pass = true
+	wrup.module(module, namespace)
 })
 
-clint.on('--package', function(required){
+clint.on('command(--package)', function(required){
 	pass = true
 	wrup.package(required)
 })
 
 var options = {}
 
-clint.on('--wrup', function(result){
+clint.on('command(--wrup)', function(result){
 	options.wrup = result;
 })
 
-clint.on('--globalize', function(result){
+clint.on('command(--globalize)', function(result){
 	options.globalize = result;
 })
 
-clint.on('--compress', function(result){
+clint.on('command(--compress)', function(result){
 	options.compress = result;
 })
 
 var fileName;
 
-clint.on('--output', function(fn){
+clint.on('command(--output)', function(fn){
 	fileName = fn
 })
 
@@ -106,11 +139,10 @@ clint.on('complete', function(){
 
 		if (fileName){
 			fs.writeFileSync(fileName, result)
-			console.warn("  the file " + fileName + " has been written.\n")
+			console.warn("  DONE: the file " + fileName + " has been written.\n")
 		} else {
-			console.warn("")
 			console.log(result)
-			console.warn("")
+			console.warn("\n  DONE.\n")
 		}
 
 		process.exit(0)
