6d251fe51b2e43adf6431ec5d1575a202450c813
Valerio Proietti <kamicane@gmail.com>
2012-04-18 03:08:12 +0200

    even shorter

--- includes/require.js
+++ includes/require.js
@@ -1,8 +1,8 @@
 var cache = {}, require = function(id){
     var module = cache[id]
     if (!module){
-        module = cache[id] = {exports: {}}
-        var exports = module.exports
+        module = cache[id] = {}
+        var exports = module.exports = {}
         modules[id].call(exports, require, module, exports, window)
     }
     return module.exports
