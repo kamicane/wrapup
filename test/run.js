5ca98014eccd85d8fee4e4342f0906f28a030cf5
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-11-25 18:12:59 +0100

    Add small test suite, by comparing the resulting JS.

---
+++ test/run.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var fs     = require('fs')
+var ujs    = require('uglify-js')
+var assert = require('assert')
+var path   = require('path')
+
+var parseAndWrite = function(file){
+    var code = fs.readFileSync(file, "utf-8")
+    var ast = ujs.parse(code)
+    var stream = ujs.OutputStream()
+    ast.print(stream)
+    return stream + ""
+}
+
+var relative = function(file){
+    return path.relative(process.cwd(), file)
+}
+
+exports.test = function(test){
+    var result = __dirname + '/output/' + test + '.result.js'
+    var should = __dirname + '/output/' + test + '.js'
+    var resultCode = parseAndWrite(result)
+    var shouldCode = parseAndWrite(should)
+    assert.equal(resultCode, shouldCode, relative(result) + " and " + relative(should) + " should be the same")
+    console.log(("✔ " + test + " test passed").green)
+}
