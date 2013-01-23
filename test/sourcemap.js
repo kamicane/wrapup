af900eeb35b0e9dac0ff20506b1e4f0c1d6fc959
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-01-23 01:13:04 +0100

    Fixes temp fix for sourcemapRoot option properly

--- test/sourcemap.js
+++ test/sourcemap.js
@@ -5,24 +5,35 @@ var fs     = require('fs')
 var wrup   = require('../lib/main')()
 var test   = require('./run').test
 
+var root = "http://localhost:8000"
+var url = "http://localhost:8000/test/output/sourcemap.result.map"
+
 wrup.options({
     output: __dirname + "/output/sourcemap.result.js",
     sourcemap: __dirname + "/output/sourcemap.result.map",
-    sourcemapRoot: "http://localhost:8000",
-    sourcemapURL: "http://localhost:8000/test/output/sourcemap.result.map"
+    sourcemapRoot: root,
+    sourcemapURL: url
 })
 
 wrup.on("error", function(err){
     assert.fail(err, undefined, "no errors should occur")
 })
 
-wrup.require(__dirname + '/fixtures/e').up(function(err, code){
+wrup.require(__dirname + '/fixtures/up').up(function(err, code){
     assert.ifError(err)
-    var stat = fs.statSync(__dirname + '/output/sourcemap.result.map')
-    assert.ok(stat.size > 0)
-    assert.ok(code.indexOf('sourceMappingURL=http://localhost:8000/test/output/sourcemap.result.map') != -1,
+
+    assert.ok(code.indexOf('sourceMappingURL=' + url) != -1,
         'should have correct sourceMappingURL')
-    test('sourcemap')
+
+    var file = __dirname + '/output/sourcemap.result.map'
+    fs.readFile(file, "utf-8", function(err, json){
+        assert.ifError(err)
+        var map = JSON.parse(json)
+        assert.equal(map.sourceRoot, root, "sourcRoot should be present")
+        assert.equal(map.sources.length, 3, "sources array should contain the original files")
+
+        test('sourcemap')
+    })
 })
 
 // To see if it actually works in a browser, run
